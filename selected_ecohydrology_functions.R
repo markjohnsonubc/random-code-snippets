eebd72b56e6b04418cdc2a1556d57eccedc6f724
markjohnsonubc <markjohnsonubc@users.noreply.github.com>
2022-06-16 16:43:33 -0700

    Add files via upload

---
+++ selected_ecohydrology_functions.R
@@ -0,0 +1,20 @@
+# source functions needed for PET_fromTemp.R
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/PET_fromTemp.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/EstCloudiness.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/transmissivity.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/NetRad.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/Solar.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/PotentialSolar.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/declination.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/slopefactor.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/solarangle.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/Longwave.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/AtmosphericEmissivity.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/PTpet.R")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/SatVapPresSlope.R")
+
+# source functions needed for Baseflow Separation
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/BaseflowSeparation.R")
+
+# source functions needed for hydrograph plotting
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/hydrograph.R")
