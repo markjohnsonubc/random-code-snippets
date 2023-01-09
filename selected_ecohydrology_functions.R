de66476be7c3b13e36da8c9e9f56f9878d96a40a
markjohnsonubc <markjohnsonubc@users.noreply.github.com>
2023-01-09 12:32:10 -0800

    Update selected_ecohydrology_functions.R

--- selected_ecohydrology_functions.R
+++ selected_ecohydrology_functions.R
@@ -1,3 +1,7 @@
+# source functions needed for hydrograph plotting
+source("https://github.com/cran/EcoHydRology/raw/master/data/OwascoInlet.rda")
+source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/hydrograph.R")
+
 # source functions needed for PET_fromTemp.R
 source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/PET_fromTemp.R")
 source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/EstCloudiness.R")
@@ -15,6 +19,3 @@ source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16
 
 # source functions needed for Baseflow Separation
 source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/BaseflowSeparation.R")
-
-# source functions needed for hydrograph plotting
-source("https://raw.githubusercontent.com/cran/EcoHydRology/d152909c12e6bb0c1f16b987aa7f49737cdcf3d3/R/hydrograph.R")
